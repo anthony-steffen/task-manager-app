d5ba7e93a5bdff31a84d4b3adaeb3ce1220c5542
anthony <anthony.steffen@outlook.com.br>
2024-10-02 12:25:00 -0300

    :heavy_plus_sign: build: Instaling dependencies and configuring package.json and index.js to the server with express

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,6 +1,8 @@
+// Initiate the server
+
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -8,6 +10,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
