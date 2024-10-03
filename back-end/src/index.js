b7c5c03a7e5c5df092fac67fc859343124f6c704
anthony <anthony.steffen@outlook.com.br>
2024-10-03 16:21:23 -0300

    :sparkles: feat: creating container mysql widh docker-compose

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,15 +1,17 @@
 // Initiate the server
+require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`congratulations, your server is running on port ${process.env.PORT}`);
 });
